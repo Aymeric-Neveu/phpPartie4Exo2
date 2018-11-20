74993730fabd3552c08e3c06e406bd5869ea226f
root <emploi.neveu@gmail.com>
2018-11-20 15:57:44 +0100

    exoé

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    function phrase($String)
+    {
+      return $String;
+    }
+    echo phrase('Hello');
+    ?>
+  </body>
+</html>
